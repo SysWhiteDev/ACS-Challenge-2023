405dc5a0f309337b9fc49255513177279c0662a6
resonanceee <res0nancee@pm.me>
2023-08-03 12:52:51 +0200

    uhh

--- components/widgets/tipsWidget.vue
+++ components/widgets/tipsWidget.vue
@@ -2,12 +2,15 @@
   <div class="widget">
     <p class="ttitle">Tips to reduce environmental impact 💡</p>
     <div>
-      <p class="cycling-text">{{ cyclingText }}</p>
+      <p class="cycling-text" >{{ cyclingText }}</p>
     </div>
   </div>
 </template>
 
 <script>
+
+let tips = ['Guida a una velocità costante ed evita frenate brusche.', 'Fahre mit konstanter Geschwindigkeit und vermeide abruptes Bremsen.', 'Spegni il motore quando rimani fermo per più di un minuto.', 'Schalte den Motor aus, wenn du länger als eine Minute im Leerlauf stehst.', 'Controlla regolarmente la pressione degli pneumatici.', 'Überprüfe regelmäßig den Reifendruck.', 'Pianifica gli spostamenti in modo efficiente per ridurre il chilometraggio.', 'Plane deine Fahrten effizient, um die Kilometerleistung zu minimieren.', 'Evita gli ingorghi nelle ore di punta, se possibile.', 'Vermeide den Berufsverkehr, wenn möglich.', 'Considera il carpooling o il ridesharing.', 'Erwäge Fahrgemeinschaften oder Mitfahrgelegenheiten.']
+
 export default {
   data() {
     return {
@@ -17,11 +20,11 @@ export default {
   mounted() {
     setInterval(() => {
       this.cycleText();
-    }, 6900);
+    }, 600);
   },
   methods: {
     cycleText() {
-      const texts = ['Guida a una velocità costante ed evita frenate brusche.', 'Fahre mit konstanter Geschwindigkeit und vermeide abruptes Bremsen.', 'Spegni il motore quando rimani fermo per più di un minuto.', 'Schalte den Motor aus, wenn du länger als eine Minute im Leerlauf stehst.', 'Controlla regolarmente la pressione degli pneumatici.', 'Überprüfe regelmäßig den Reifendruck.', 'Pianifica gli spostamenti in modo efficiente per ridurre il chilometraggio.', 'Plane deine Fahrten effizient, um die Kilometerleistung zu minimieren.', 'Evita gli ingorghi nelle ore di punta, se possibile.', 'Vermeide den Berufsverkehr, wenn möglich.', 'Considera il carpooling o il ridesharing.', 'Erwäge Fahrgemeinschaften oder Mitfahrgelegenheiten.'];
+      const texts = tips;
       const currentIndex = texts.indexOf(this.cyclingText);
       this.cyclingText = texts[(currentIndex + 1) % texts.length];
     }
